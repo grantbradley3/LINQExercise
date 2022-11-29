cd7ade4ebe58702c8512d62f00705d1aab262d3b
Grant Bradley <grantbradleytiger13@gmail.com>
2022-11-29 11:48:10 -0500

    Initial Commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> vGames = new List<string>
+
+            {
+                "Elden Ring",
+                "League of Legends",
+                "Overwatch",
+                "CSGO",
+                "Far Cry",
+                "Assassins Creed",
+                "Mario World",
+                "Mario Kart",
+                "Forza Motorsports"
+            };
+            var order = vGames.OrderBy(x => x.Length).ToList();
+
+
+            foreach(var game in order) 
+            {
+                Console.WriteLine(game);
+
+            }
         }
     }
 }
